e96ab0f2b0accaec7f8bddc2d5792ebf77f4cbf5
Flying Code Solutions <110687855+flying-code-solutions@users.noreply.github.com>
2022-08-14 23:58:50 +0200

    add data fetching on component, then replace by getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import products from '../public/products.json';
+
 function Home() {
+  // const [products, setProducts] = useState([]);
+
+  // useEffect(() => {
+  //   getProducts();
+  // }, []);
+
+  // async function getProducts() {
+  //   // todo use fetch iso axios
+  //   const url = 'http://localhost:3000/api/products';
+  //   const response = await axios.get(url);
+
+  //   console.log(response.data);
+  // }
+
   return <>Home</>
+}
+
+export async function getServerSideProps(context) {
+  // fetch data on server
+  //
+  return {
+    props: {
+      products: products
+    }
+  };
+
 }
 
 export default Home;
