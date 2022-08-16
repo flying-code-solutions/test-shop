c39c3d26d339fb028ca8aca4dbaf7533cd57fdd8
Flying Code Solutions <110687855+flying-code-solutions@users.noreply.github.com>
2022-08-16 20:25:14 +0200

    implement product detail view

---
+++ pages/product.js
@@ -0,0 +1,29 @@
+import connectDb from "../utils/connectDb";
+import ProductModel from "../models/Product";
+import ProductSummary from "../components/Product/ProductSummary";
+import ProductAttributes from "../components/Product/ProductAttributes";
+
+function Product({ product }) {
+  // the whole props object can be passed with spread operator like this
+  return (
+    <>
+      <ProductSummary {...product} />
+      <ProductAttributes {...product} />
+    </>
+  );
+}
+
+// context object includes URL query string as an object of key value pairs
+export async function getServerSideProps({ query: { _id: id } }) {
+  connectDb();
+  const product = await ProductModel.findById(id);
+  console.log(product);
+
+  return {
+    props: {
+      product: JSON.parse(JSON.stringify(product))
+    }
+  };
+}
+
+export default Product;
