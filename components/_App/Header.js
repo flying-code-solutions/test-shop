d8a2955bdb5443026557a1e6dd0f59a194902b4e
Flying Code Solutions <110687855+flying-code-solutions@users.noreply.github.com>
2022-08-17 21:38:21 +0200

    implement product form

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import nprogress from 'nprogress';
-import { Menu, Container, Icon } from 'semantic-ui-react';
+import { useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import nprogress from "nprogress";
+import { Menu, Container, Icon } from "semantic-ui-react";
 
 function Header() {
   // hooks need to be executed at the very top
@@ -11,56 +11,47 @@ function Header() {
     const handleStart = (url) => {
       console.log(`Loading: ${url}`);
       nprogress.start();
-    }
+    };
     const handleStop = () => {
       nprogress.done();
-    }
+    };
 
-    router.events.on('routeChangeStart', handleStart);
-    router.events.on('routeChangeComplete', handleStop);
-    router.events.on('routeChangeError', handleStop);
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
 
     return () => {
-      router.events.off('routeChangeStart', handleStart);
-      router.events.off('routeChangeComplete', handleStop);
-      router.events.off('routeChangeError', handleStop);
-    }
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
   }, [router]);
 
-  const user = false;
+  const user = true;
 
   function isActive(route) {
     return router.pathname === route;
   }
 
   return (
-    <Menu fluid id="menu" inverted>
+    <Menu fluid id="menu" inverted stackable>
       <Container text>
         <Link href="/">
           <Menu.Item header active={isActive("/")}>
-            <Icon 
-              name="shopping bag"
-              size="large"
-            />
+            <Icon name="shopping bag" size="large" />
             Test Shop
           </Menu.Item>
         </Link>
         <Link href="/cart">
           <Menu.Item header active={isActive("/cart")}>
-            <Icon 
-              name="cart"
-              size="large"
-            />
+            <Icon name="cart" size="large" />
             Cart
           </Menu.Item>
         </Link>
         {user && (
           <Link href="/create">
             <Menu.Item header active={isActive("/create")}>
-              <Icon
-                name="add square"
-                size="large"
-              />
+              <Icon name="add square" size="large" />
               Create
             </Menu.Item>
           </Link>
@@ -69,18 +60,12 @@ function Header() {
           <>
             <Link href="/account">
               <Menu.Item header active={isActive("/account")}>
-                <Icon 
-                  name="user"
-                  size="large"
-                />
+                <Icon name="user" size="large" />
                 Account
               </Menu.Item>
             </Link>
             <Menu.Item header>
-              <Icon 
-                name="sign out"
-                size="large"
-              />
+              <Icon name="sign out" size="large" />
               Logout
             </Menu.Item>
           </>
@@ -88,27 +73,21 @@ function Header() {
           <>
             <Link href="/login">
               <Menu.Item header active={isActive("/login")}>
-                <Icon 
-                  name="sign in"
-                  size="large"
-                />
+                <Icon name="sign in" size="large" />
                 Login
               </Menu.Item>
             </Link>
             <Link href="/signup">
               <Menu.Item header active={isActive("/signup")}>
-                <Icon 
-                  name="signup"
-                  size="large"
-                />
+                <Icon name="signup" size="large" />
                 Signup
               </Menu.Item>
             </Link>
           </>
-        )}  
+        )}
       </Container>
     </Menu>
-    );
+  );
 }
 
 export default Header;
--- pages/create.js
+++ pages/create.js
@@ -1,5 +1,114 @@
+import { useState } from "react";
+import {
+  Form,
+  Input,
+  TextArea,
+  Button,
+  Image,
+  Message,
+  Header,
+  Icon
+} from "semantic-ui-react";
+
+const NEW_PRODUCT = {
+  name: "",
+  price: "",
+  description: "",
+  media: ""
+};
+
 function Create() {
-  return <>Create</>;
+  const [product, setProduct] = useState(NEW_PRODUCT);
+  const [mediaPreview, setMediaPreview] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  function handleChange(event) {
+    const { name, value, files } = event.target;
+    if (name === "media") {
+      setProduct((prev) => ({ ...prev, media: files[0] }));
+      setMediaPreview(window.URL.createObjectURL(files[0]));
+    } else {
+      setProduct((prev) => ({ ...prev, [name]: value }));
+    }
+  }
+
+  function handleSubmit(event) {
+    // prevent the page from reloading after submit (default behaviour)
+    event.preventDefault();
+
+    // todo implement product submission, just logging it for now
+    console.log(product);
+    setSuccess(true);
+
+    // reset form
+    setProduct(NEW_PRODUCT);
+    // setTimeout(() => {
+    //   setSuccess(false);
+    // }, 10000);
+  }
+
+  return (
+    <>
+      <Header as="h2" block>
+        <Icon name="add" color="orange" />
+        Create New Product
+      </Header>
+      <Form success={success} onSubmit={handleSubmit}>
+        <Message
+          success
+          icon="check"
+          header="Success!"
+          content="Your product has been successfully created."
+        />
+        <Form.Group widths="equal">
+          <Form.Field
+            control={Input}
+            name="name"
+            label="Name"
+            placeholder="Name"
+            onChange={handleChange}
+            value={product.name}
+          />
+          <Form.Field
+            control={Input}
+            name="price"
+            label="Price"
+            placeholder="Price"
+            type="number"
+            min="0.00"
+            step="0.01"
+            onChange={handleChange}
+            value={product.price}
+          />
+          <Form.Field
+            control={Input}
+            name="media"
+            label="Media"
+            content="Select Image"
+            type="file"
+            accept="image/*"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Image src={mediaPreview} rounded centered size="medium" />
+        <Form.Field
+          control={TextArea}
+          name="description"
+          label="Description"
+          placeholder="Description"
+          onChange={handleChange}
+          value={product.description}
+        />
+        <Form.Field
+          control={Button}
+          color="blue"
+          icon="pencil alternate"
+          content="Submit"
+          type="submit"
+        />
+      </Form>
+    </>
+  );
 }
 
 export default Create;
