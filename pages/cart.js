4f9adf268ba1a97bf3529ff34e58ef356b83b8da
Flying Code Solutions <110687855+flying-code-solutions@users.noreply.github.com>
2022-08-19 06:51:51 +0200

    prepare page structure for cart

--- pages/cart.js
+++ pages/cart.js
@@ -1,5 +1,12 @@
+import { Segment } from 'semantic-ui-react';
+import CartItemList from '../components/Cart/CartItemList';
+import CartSummary from '../components/Cart/CartSummary.js';
+
 function Cart() {
-  return <>Cart</>;
+  return <><Segment>
+    <CartItemList />
+    <CartSummary />
+  </Segment></>;
 }
 
 export default Cart;
